82fe6888e855665447f3603ca737ae4a718a5cfc
Roberto Abril hidalgo <rabril-h@cbr10s5.42barcelona.com>
2022-02-21 19:17:43 +0100

    now header is actually norminette approved...

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: rabril-h <rabril-h@student.42barc...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 20:54:28 by rabril-h          #+#    #+#             */
-/*   Updated: 2022/02/21 18:46:05 by rabril-h         ###   ########.bcn      */
+/*   Updated: 2022/02/21 19:16:40 by rabril-h         ###   ########.bcn      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s1);
@@ -24,7 +24,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	*ft_calloc(size_t count, size_t size);
-size_t  ft_strlenn(const char *s, char c);
-	
-#endif
+size_t	ft_strlenn(const char *s, char c);
 
+#endif
